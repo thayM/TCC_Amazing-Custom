db71da4ce8e5b4facb358e472ade0493a2888289
Kaike grando <117050470+kaikeBG@users.noreply.github.com>
2023-08-09 01:15:47 -0300

    feature html cadFragrancia

---
+++ Admin/appAdmin/cadFragrancia.php
@@ -0,0 +1,38 @@
+<?php
+  include_once('../components/header.php');
+?>
+<head>
+    <title>cadFragancia</title>
+</head>
+<body>
+    <div>
+        <div>
+            <input type="text">
+            <button>lupa</button>
+        </div>
+        <button>add Fragancia</button>
+    </div>
+    <div>
+        <div>
+            <div>
+                <h3>Gold</h3>
+                <p>tom amadeirado, feminino</p>
+            </div>
+            <div>
+                <button>lapis</button>
+                <button>lixeira</button>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h3>Gold</h3>
+                <p>tom amadeirado, feminino</p>
+            </div>
+            <div>
+                <button>lapis</button>
+                <button>lixeira</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
