f5a95b39822deea32ad539a921b836a18a78477b
thayM <thay.contato1@gmail.com>
2023-08-16 21:13:35 -0300

    fix css page da página de login do adm

--- Admin/index.php
+++ Admin/index.php
@@ -1,4 +1,4 @@
-<header class="d-flex align-items-center justify-content-between">
+<!-- <header class="d-flex align-items-center justify-content-between">
   <a href="./appAdmin/home.php">
     <img src="../assets/img/Logo.png" alt="Logo" class="logo">
   </a>
@@ -11,16 +11,36 @@
       <li><a href="./appAdmin/home.php" class="item">Listagem Pedidos</a></li>
     </ul>
   </nav>
-</header>
+</header> -->
 
-<form action="./functions/func_login.php" method="POST">
-  <div>
-    <input type="text" name="nome" id="nome">
-    <label for="nome">Nome:</label>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style_login.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <title>Login Page</title>
+</head>
+<body>
+  
+  <div class="imgLeao">
+    <img class="logoLeao" src="../assets/img/leaoamazingLogo.png" alt="brasãoLeão">
   </div>
-  <div>
-    <input type="password" name="senha" id="senha">
-    <label for="senha">Senha:</label>
+
+  <div class="campos">
+    <img class="logo" src="assets/icons/Logo.png" alt="logo"></img>
+    <form action="./functions/func_login.php" method="POST">
+      <div>
+        <input type="text" name="nome" id="nome" placeholder="Nome">
+      </div>
+      <div>
+        <input type="password" name="senha" id="senha" placeholder="Senha">
+      </div>
+      <button class="btn" type="submit">ENTRAR</button>
+    </form>
   </div>
-  <button type="submit">Logar</button>
-</form>
+
+</body>
+</html>
+
