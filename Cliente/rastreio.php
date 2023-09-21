8d7c1f8a02f570114d0b865ee75c2e16550614ab
thayM <thay.contato1@gmail.com>
2023-09-21 10:32:42 -0300

    fix css modal da página do cliente

--- Cliente/rastreio.php
+++ Cliente/rastreio.php
@@ -31,20 +31,21 @@
                 </div>
             </div>
 
+
             <div class="etapas">
-                <div class="etapaPag">
+                <div class="etapa etapaPag">
                     <img class="icons" src="assets/img/fi-sr-dollar.svg" alt="">
                     <p>Pagamento</p>
                 </div>
-                <div class="etapaArt">
+                <div class="etapa etapaArt">
                     <img class="icons" src="assets/img/fi-sr-edit-alt.svg" alt="">
                     <p>Arte</p>
                 </div>
-                <div class="etapaProd">
+                <div class="etapa etapaProd">
                     <img class="icons" src="assets/img/fi-sr-settings.svg" alt="">
                     <p>Produção</p>
                 </div>
-                <div class="etapaEnvio">
+                <div class="etapa etapaEnvio">
                     <img class="icons" src="assets/img/Vector.svg" alt="">
                     <p>Envio</p>
                 </div>
@@ -61,17 +62,34 @@
 
                 <div class="endereco">
                     <div class="infos">
-                    <p>Rua:</p>
-                    <p>N°:</p>
-                    <p>Bairro:</p>
-                    <p>Cidade:</p>
-                    <p>CEP:</p>
-                    <p>Complemento:</p>
+                        <div class="ruaInfo">
+                            <h3>Rua:</h3>
+                            <p></p>
+                        </div>
+                        <div class="numeroInfo">
+                            <h3>N°:</h3>
+                            <p></p>
+                        </div>
+                        <div class="bairroInfo">
+                            <h3>Bairro:</h3>
+                            <p></p>
+                        </div>
+                        <div class="cidadeInfo">
+                            <h3>Cidade:</h3>
+                            <p></p>
+                        </div>
+                        <div class="cepInfo">
+                            <h3>CEP:</h3>
+                            <p></p>
+                        </div>
+                        <div class="complementoInfo">
+                            <h3>Complemento:</h3>
+                            <p></p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
         <img onclick="removeStyle()" src="../Admin/assets/icons/x.svg" alt="fechar" class="fechar fecharCad">
     </div>
 </div>
@@ -97,7 +115,6 @@
                 <p class="etapa">Arte finalizada</p>
             </div>
         </div>
-
         <div class="card-body">
             <div class="card-title">
                 <h3 class="titulo">MODELO</h3>
@@ -106,14 +123,12 @@
                     <p class="card-text">Quadrado</p>
                 </div>
             </div>
-
             <div class="card-title">
                 <h3 class="titulo">FRAGRÂNCIA</h3>
                 <div class="cardInfo">
                     <p class="card-text">Gold</p>
                 </div>
             </div>
-
             <div class="card-title">
                 <h3 class="titulo">QUANTIDADE</h3>
                 <div class="cardInfo">
@@ -123,12 +138,11 @@
         </div>
     </div>
 </main>
+
 <!-- JS BOOTSTRAP -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <!-- JQUERY -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-
 <script src="../Admin/assets/js/style.js"></script>
 <script src="../Admin/assets/js/modal.js"></script>
-
 </body>
