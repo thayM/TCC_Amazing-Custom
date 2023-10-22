e91d05ba4df15eaf81074e2fb7498cabca46b66e
thayM <thay.contato1@gmail.com>
2023-10-22 18:46:12 -0300

    ajuste função excluir pedidos

--- Admin/appAdmin/functions/excluir/func_excluirPed.php
+++ Admin/appAdmin/functions/excluir/func_excluirPed.php
@@ -6,5 +6,22 @@ $sqlDelete = "DELETE FROM pedido WHERE cod_ped = :id";
 $stmt = $conn->prepare($sqlDelete);
 $stmt->bindValue(":id", $id);
 $stmt->execute();
+
+$sqlSelectPedPro = "SELECT fkcod_prod FROM pedido_produto WHERE fkcod_ped = :id";
+$sqlPedido_produto = $conn->prepare($sqlSelectPedPro);
+$sqlPedido_produto->bindValue(":id", $id); 
+$sqlPedido_produto->execute();
+$codProd = $sqlPedido_produto->fetch(PDO::FETCH_OBJ);
+$codProd = $codProd->fkcod_prod;
+
+$sqlDeletePP = "DELETE FROM pedido_produto WHERE fkcod_ped = :id";
+$stmt = $conn->prepare($sqlDeletePP);
+$stmt->bindValue(":id", $id);
+$stmt->execute();
+
+$sqlDeleteProd = "DELETE FROM produto WHERE cod_prod = :id";
+$stmt = $conn->prepare($sqlDeleteProd);
+$stmt->bindValue(":id", $codProd);
+$stmt->execute();
 ?>
 <meta http-equiv="refresh" content="0; url=../../home.php">
