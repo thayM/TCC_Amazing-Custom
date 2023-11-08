6aee8138a979047a9695fa57d2becccbaa22d68e
thayM <thay.contato1@gmail.com>
2023-11-08 20:44:45 -0300

    add func login cliente

--- Cliente/codRastreio.php
+++ Cliente/codRastreio.php
@@ -14,10 +14,12 @@
     <img src="assets/img/wepik-export-20230522104437k8p5 1.png" class="img" alt="ilustração">
     <div class="modal-body">
         <h2 class="titulo">Bem-vindo(a) ao rastreamento!</h2>
+        <form action="functions/func_rastreio.php" method="post">
         <div class="search">
-            <input type="text" name="" id="barraBusca" placeholder="Cole seu código aqui...">
+            <input type="text" name="codClie" id="barraBusca" placeholder="Cole seu código aqui...">
             <button class="btnCod"><img src="../assets/img/lupa.svg" alt=""></button>
         </div>
+        </form>
     </div>
 </div>
 
---
+++ Cliente/functions/func_rastreio.php
@@ -0,0 +1,23 @@
+<?php 
+
+require_once("../../lib/conn.php");
+extract($_POST);
+
+$verificarLogin = "SELECT * FROM cliente WHERE cod_rastreamento = :cod";
+$verificarCliente = $conn->prepare($verificarLogin);
+$verificarCliente-> bindValue(":cod",$codClie);
+$verificarCliente-> execute();
+
+if ($verificarCliente->rowCount() === 1) {
+    $cliente = $verificarCliente->fetch(PDO::FETCH_OBJ);
+    $idCliente = $cliente->cod_cli;
+    session_start();
+    
+    $_SESSION['loggIn'] = true;
+    header("Location: ../rastreio.php?id=$idCliente");
+}else{
+    header("Location: ../codRastreio.php");
+}
+
+
+?>
