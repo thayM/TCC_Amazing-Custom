a84ddd155a21d23c801a82d3f6ef3d1a92749bfe
mygk-bea <meyaguskobeatriz@gmail.com>
2023-11-15 23:04:14 -0300

    feature footer

--- Cliente/appCliente/rastreio.php
+++ Cliente/appCliente/rastreio.php
@@ -33,6 +33,9 @@ $subtotal = 0;
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/9dabb0ed4f.js" crossorigin="anonymous"></script>
+
     <link rel="shortcut icon" href="../../assets/img/favicon.ico">
     <link rel="stylesheet" href="../../assets/css/style.css">
     <link rel="stylesheet" href="../../assets/css/style_rastreamento.css">
@@ -91,7 +94,7 @@ $subtotal = 0;
                     ?>
                         <div class="cardInfo">
                             <div class="cardInfoInner d-flex justify-content-between align-items-center">
-                                <div class="imgModelo d-flex align-items-center">
+                                <div class="imgModelo d-flex align-items-center justify-content-center">
                                     <img class="produto_img" src="../../upload/<?= $produto->nomeArq_modelo ?>" alt="">
                                 </div>
                                 <p class="card-text"><?= $produto->nome_modelo ?></p>
@@ -137,6 +140,59 @@ $subtotal = 0;
         ?>
     </main>
 
+    <footer class="d-flex justify-content-around">
+        <div class="footer__container d-flex justify-content-between flex-wrap">
+            <div>
+                <h3 class="title">Siga nossas redes:</h3>
+                <div class="footer__content d-flex flex-column">
+                    <a href="https://www.instagram.com/amazingcustom_perfumecar" target="_blank">
+                        <i class="fa-brands fa-instagram"></i>
+                        amazingcustom_perfumecar
+                    </a>
+                    <a href="https://www.facebook.com/AmazingCustomPerfumeCar" target="_blank">
+                        <i class="fa-brands fa-facebook-f"></i>
+                        Amazing Custom Perfume Car
+                    </a>
+                </div>
+            </div>
+            <div>
+                <h3 class="title">Contato:</h3>
+                <div class="footer__content d-flex flex-column">
+                    <a href="">
+                        <i class="fa-brands fa-whatsapp"></i>
+                        (15) 98162-2454
+                    </a>
+                    <a href="">
+                        <i class="fa-regular fa-envelope"></i>
+                        atendimento@amazingcustom.com.br
+                    </a>
+                </div>
+            </div>
+            <div>
+                <h3 class="title">Acesse nosso site:</h3>
+                <div class="footer__content">
+                    <a href="https://www.amazingcustom.com.br/" target="_blank">
+                        <i class="fa-solid fa-link"></i>
+                        amazingcustom.com.br
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="footer__container">
+            <h3 class="title">Desenvolvido por:</h3>
+            <div class="footer__content d-flex flex-column">
+                <a href="https://github.com/thayM" target="_blank">Thayná Marostica</a>
+                <a href="https://github.com/mygk-bea" target="_blank">Beatriz Meyagusko</a>
+                <a href="https://github.com/kaikeBG" target="_blank">Kaike Bulsing</a>
+                <a href="https://github.com/aguiarhub" target="_blank">Pedro Aguiar</a>
+            </div>
+        </div>
+
+        <div class="footer_container d-flex align-items-center justify-content-between">
+            <img src="../../assets/img/Logo_leao.png" alt="logo-leao" class="footer__logo-leao">
+            <img src="../../assets/img/Logo.png" alt="logo" class="footer__logo">
+        </div>
+    </footer>
     <!-- JQUERY -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="../../Admin/assets/js/style.js"></script>
--- Cliente/index.php
+++ Cliente/index.php
@@ -9,6 +9,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/9dabb0ed4f.js" crossorigin="anonymous"></script>
+
 	<link rel="shortcut icon" href="../assets/img/favicon.ico">
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="./assets/css/style_modal.css">
@@ -33,5 +36,59 @@
             </form>
         </div>
     </div>
+    
+    <footer class="d-flex justify-content-around">
+        <div class="footer__container d-flex justify-content-between flex-wrap">
+            <div>
+                <h3 class="title">Siga nossas redes:</h3>
+                <div class="footer__content d-flex flex-column">
+                    <a href="https://www.instagram.com/amazingcustom_perfumecar" target="_blank">
+                        <i class="fa-brands fa-instagram"></i>
+                        amazingcustom_perfumecar
+                    </a>
+                    <a href="https://www.facebook.com/AmazingCustomPerfumeCar" target="_blank">
+                        <i class="fa-brands fa-facebook-f"></i>
+                        Amazing Custom Perfume Car
+                    </a>
+                </div>
+            </div>
+            <div>
+                <h3 class="title">Contato:</h3>
+                <div class="footer__content d-flex flex-column">
+                    <a href="">
+                        <i class="fa-brands fa-whatsapp"></i>
+                        (15) 98162-2454
+                    </a>
+                    <a href="">
+                        <i class="fa-regular fa-envelope"></i>
+                        atendimento@amazingcustom.com.br
+                    </a>
+                </div>
+            </div>
+            <div>
+                <h3 class="title">Acesse nosso site:</h3>
+                <div class="footer__content">
+                    <a href="https://www.amazingcustom.com.br/" target="_blank">
+                        <i class="fa-solid fa-link"></i>
+                        amazingcustom.com.br
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="footer__container">
+            <h3 class="title">Desenvolvido por:</h3>
+            <div class="footer__content d-flex flex-column">
+                <a href="https://github.com/thayM" target="_blank">Thayná Marostica</a>
+                <a href="https://github.com/mygk-bea" target="_blank">Beatriz Meyagusko</a>
+                <a href="https://github.com/kaikeBG" target="_blank">Kaike Bulsing</a>
+                <a href="https://github.com/aguiarhub" target="_blank">Pedro Aguiar</a>
+            </div>
+        </div>
+
+        <div class="footer_container d-flex align-items-center justify-content-between">
+            <img src="../assets/img/Logo_leao.png" alt="logo-leao" class="footer__logo-leao">
+            <img src="../assets/img/Logo.png" alt="logo" class="footer__logo">
+        </div>
+    </footer>
 </body>
 </html>
