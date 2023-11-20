1958eb7524f2b70177d87f03b43c09540e6e9c35
mygk-bea <meyaguskobeatriz@gmail.com>
2023-11-20 19:54:00 -0300

    feature validacao cadCliente

--- Admin/appAdmin/cadCliente.php
+++ Admin/appAdmin/cadCliente.php
@@ -1,6 +1,13 @@
 <?php
-  include_once('../components/header.php');
+include_once('../components/header.php');
+session_start();
+$errors = $_SESSION['errors'] ?? null;
+unset($_SESSION['errors']);
+
+$valores = $_SESSION['valores'] ?? null;
+unset($_SESSION['valores']);
 ?>
+
 <head>
   <link rel="stylesheet" href="../assets/css/style_cadCliente.css">
   <title>Cadastro Cliente</title>
@@ -10,32 +17,69 @@
   <div class="container_form d-flex flex-column align-items-center">
     <form action="./functions/func_cadClie.php" method="post" class="content_form d-flex flex-column">
       <h3>Novo Cliente</h3>
-      <input type="text"name="nome" placeholder="Nome Completo">
-      <input type="email" name="email"placeholder="E-mail">
+
+      <div class="input__field d-flex flex-column">
+        <input type="text" name="nome" placeholder="Nome Completo" value="<?= ($valores) ? $valores['nome'] : null ?>">
+        <span class="error"><?= (isset($errors['nome'])) ? $errors["nome"] : null ?></span>
+      </div>
+
+      <div class="input__field d-flex flex-column">
+        <input type="email" name="email" placeholder="E-mail" value="<?= ($valores) ? $valores['email'] : null ?>">
+        <span class="error"><?= (isset($errors['email'])) ? $errors["email"] : null ?></span>
+      </div>
 
       <div class="d-flex justify-content-between">
         <label for="telefone-1">Telefone:</label>
-        <input type="text" name="telefone" id="telefone-1" class="info_tel-input">
+        <div class="info_tel input__field d-flex flex-column">
+          <input type="text" name="telefone" id="telefone-1" class="info_tel-input" value="<?= ($valores) ? $valores['telefone'] : null ?>">
+          <span class="error"><?= (isset($errors['telefone'])) ? $errors["telefone"] : null ?></span>
+        </div>
 
         <label for="telefone-2">Tel Opcional:</label>
-        <input type="text" name="telefone2" id="telefone-2" class="info_tel-input">
+        <div class="info_tel input__field d-flex flex-column">
+          <input type="text" name="telefone_opcional" id="telefone-2" class="info_tel-input" value="<?= ($valores) ? $valores['telefone_opcional'] : null ?>">
+          <span class="error"><?= (isset($errors['telefone_opcional'])) ? $errors["telefone_opcional"] : null ?></span>
+        </div>
       </div>
 
       <div class="endereco d-flex flex-wrap">
-        <input type="text" name="cep" class="cep" onchange="pesquisacep(this.value)" onkeyup="pesquisacep(this.value)">
+        <div class="input__field d-flex flex-column">
+          <input type="text" name="cep" class="cep" onchange="pesquisacep(this.value)" onkeyup="pesquisacep(this.value)" value="<?= ($valores) ? $valores['cep'] : null ?>">
+          <span class="error"><?= (isset($errors['cep'])) ? $errors["cep"] : null ?></span>
+        </div>
+
         <a href="https://buscacepinter.correios.com.br/app/endereco/index.php" class="endereco_buscar-cep">
           <p>Não sabe o CEP?</p>
         </a>
 
         <div class="endereco_content d-flex justify-content-between">
-          <input type="text" name="logradouro" id="logradouro__cli" placeholder="Logradouro">
-          <input type="text" name="num"  placeholder="Número">
-          <input type="text" name="bairro"  id="bairro__cli" placeholder="Bairro">
+          <div class="input__field d-flex flex-column">
+            <input type="text" name="logradouro" id="logradouro__cli" placeholder="Logradouro" value="<?= ($valores) ? $valores['logradouro'] : null ?>">
+            <span class="error"><?= (isset($errors['logradouro'])) ? $errors["logradouro"] : null ?></span>
+          </div>
+          <div class="input__field d-flex flex-column">
+            <input type="text" name="numero" placeholder="Número" value="<?= ($valores) ? $valores['numero'] : null ?>">
+            <span class="error"><?= (isset($errors['numero'])) ? $errors["numero"] : null ?></span>
+          </div>
+          <div class="input__field d-flex flex-column">
+            <input type="text" name="bairro" id="bairro__cli" placeholder="Bairro" value="<?= ($valores) ? $valores['bairro'] : null ?>">
+            <span class="error"><?= (isset($errors['bairro'])) ? $errors["bairro"] : null ?></span>
+          </div>
         </div>
+
         <div class="endereco_content d-flex justify-content-between">
-          <input type="text" name="cidade"  id="cidade__cli" placeholder="Cidade">
-          <input type="text" name="uf" id="uf__cli" placeholder="UF">
-          <input type="text" name="complemento" id="complemento__cli" placeholder="Complemento">
+          <div class="input__field d-flex flex-column">
+            <input type="text" name="cidade" id="cidade__cli" placeholder="Cidade" value="<?= ($valores) ? $valores['cidade'] : null ?>">
+            <span class="error"><?= (isset($errors['cidade'])) ? $errors["cidade"] : null ?></span>
+          </div>
+          <div class="input__field d-flex flex-column">
+            <input type="text" name="uf" id="uf__cli" placeholder="UF" maxlength="2" value="<?= ($valores) ? $valores['uf'] : null ?>">
+            <span class="error"><?= (isset($errors['uf'])) ? $errors["uf"] : null ?></span>
+          </div>
+          <div class="input__field d-flex flex-column">
+            <input type="text" name="complemento" id="complemento__cli" placeholder="Complemento" value="<?= ($valores) ? $valores['complemento'] : null ?>">
+            <span class="error"><?= (isset($errors['complemento'])) ? $errors["complemento"] : null ?></span>
+          </div>
         </div>
       </div>
       <div class="btn-cadastro d-flex justify-content-end p-0 w-100">
@@ -46,4 +90,5 @@
   <script src="../assets/js/style.js"></script>
   <script src="../assets/js/cadCliente.js"></script>
 </body>
+
 </html>
--- Admin/appAdmin/functions/func_cadClie.php
+++ Admin/appAdmin/functions/func_cadClie.php
@@ -1,14 +1,44 @@
 <?php
 require '../../../lib/conn.php';
 extract($_POST);
+session_start();
+$_SESSION['valores'] = $_POST;
 
+$errors = [];
+foreach($_POST as $indice => $valor){
+  if(empty($valor) && $indice != 'telefone_opcional' && $indice != 'complemento'){
+    $errors[$indice] = "O campo $indice é obrigatório*";
+  }
+}
+
+if(!$errors['email'] && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $erro = "E-mail deve ser no formato nome@domínio";
+}
+
+if(!$errors['cep'] && strlen($cep) < 9) {
+  $errors['cep'] = "O cep deve ter 8 caracteres.*";
+}
+
+if(!$errors['telefone'] && strlen($telefone) < 14) {
+  $errors['telefone'] = "O telefone deve ter 11 caracteres.*";
+}
+
+if(!empty($telefone_opcional) && strlen($telefone_opcional) < 14) {
+  $errors['telefone_opcional'] = "O telefone deve ter 11 caracteres.*";
+}
+
+if(!$errors['uf'] && strlen($uf) != 2 || is_numeric($uf)) {
+  $errors['uf'] = "UF inválida*";
+}
+
+if(count($errors) == 0) {
 $tel = $telefone."/".$telefone2;
 
 $sqlInsert='INSERT INTO endereco VALUES(0,:cep,:logradouro,:num,:cidade,:bairro,:uf,:complemento)';
 $stmt = $conn->prepare($sqlInsert);
 $stmt->bindValue(":cep", $cep);
 $stmt->bindValue(":logradouro", $logradouro);
-$stmt->bindValue(":num", $num);
+$stmt->bindValue(":num", $numero);
 $stmt->bindValue(":cidade", $cidade);
 $stmt->bindValue(":bairro", $bairro);
 $stmt->bindValue(":uf", $uf);
@@ -17,7 +47,6 @@ $stmt->execute();
 
 $codEnd = $conn->lastInsertId();
 
-
 $sqlInsert='INSERT INTO cliente VALUES(0, :idEnd, :nome, "123",:tel,:email)';
 $stmt = $conn->prepare($sqlInsert);
 $stmt->bindValue(":nome", $nome);
@@ -25,5 +54,10 @@ $stmt->bindValue(":idEnd", $codEnd);
 $stmt->bindValue(":tel", $tel);
 $stmt->bindValue(":email", $email);
 $stmt->execute();
+
+header('Location: ../listagemCli.php');
+} else {
+  $_SESSION['errors'] = $errors; 
+  header('Location: ../cadCliente.php');
+}
 ?>
-<meta http-equiv="refresh" content="0; url=../listagemCli.php">
